45789b1fa5bc68d59aa01b30871d931502919578
eric.rapp <eric.rapp@MU-C1-01.mullen.pvt.k12.co.us>
2018-11-15 14:53:37 -0700

    simple conversion in a different project

--- Github Group Project/src/HelloWorld.java
+++ Github Group Project/src/HelloWorld.java
@@ -9,46 +9,6 @@ public class HelloWorld
 				
 				
 				
-				//ERIC CALCULATOR
-				ArrayList<Calculator>calculationHistory = new ArrayList<Calculator>();
-				boolean run = true;
-				
-				while(run == true)
-				{
-				System.out.println("Please type in an equation that you would like to know?");
-				Scanner userInput = new Scanner(System.in);
-				String equation = userInput.nextLine();
-				String[] equationArray = equation.split(" ");
-				double numberOne = Integer.parseInt(equationArray[0]);
-				double numberTwo = Integer.parseInt(equationArray[2]);
-				String operator = equationArray[1];
-				Calculator c = new Calculator(numberOne, operator, numberTwo);
-				switch(operator)
-				{
-				case "+":
-				{
-					c.add(numberOne,numberTwo);
-					break;
-				}
-				case "-":
-				{
-					c.subtract(numberOne, numberTwo);
-					break;
-				}	
-				case "/":
-					
-				{
-					c.divide(numberOne, numberTwo);
-					break;
-				}
-				case "*":
-				{
-					c.multiply(numberOne, numberTwo);
-					break;
-				}
-			}
-				
-			}
 			}
 	}
 
